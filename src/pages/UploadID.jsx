37dac67f3c315d10f28422c87e098b08f21376f0
justin2150 <kolskidman@gmail.com>
2023-12-20 00:56:51 -0600

    updated commit

--- src/pages/UploadID.jsx
+++
@@ -1,9 +0,0 @@
-import FrontID from "../components/FrontID";
-import styles from "./UploadID.module.css";
-export default function UploadID() {
-  return (
-    <main className={styles.main}>
-      <FrontID />
-    </main>
-  );
-}
