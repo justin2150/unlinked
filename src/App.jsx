dce88c4c869a5a49feefec79ea224c6fbb2c5eb0
justin Rosenbaum <justinrose2150@gmail.com>
2024-03-26 05:54:00 -0500

    updated commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import UploadID from './pages/UploadID';
 import IdmePage from './pages/IdmePage';
-import IncomePage from './pages/IncomePage';
-import BankPage from './pages/BankPage';
 import FinishPage from './pages/FinishPage';
 import './App.css';
 
@@ -14,8 +12,6 @@ export default function App() {
         <Route path="register" element={<RegisterPage />} />
         <Route path="/upload-id" element={<UploadID />} />
         <Route path="/idme" element={<IdmePage />} />
-        <Route path="income" element={<IncomePage />} />
-        <Route path="banks" element={<BankPage />} />
         <Route path="finish" element={<FinishPage />} />
       </Routes>
     </BrowserRouter>
